acee7de69345b2fd1a19f947abd30b041bdd8edf
yoshi <yoshimain016@gmail.com>
2023-10-09 12:03:24 +0900

    create view

--- src/view/PostPage.tsx
+++ src/view/PostPage.tsx
@@ -4,27 +4,75 @@ import SpaceBox from "./components/layout/SpaceBox";
 import Back from "./components/basic/Back";
 import Column from "./components/layout/Column";
 import { usePostPrompt } from "../usecase/prompt_use_case";
-import Row from "./components/layout/Row";
-import Icon from "./components/basic/Icon";
-import GrowSpace from "./components/layout/GrowSpace";
-import { useRef, useState } from "react";
+import { useState } from "react";
 import Card from "./components/basic/Card";
 
 
 const PostTitle = styled.div`
+    font-family  : 'Kosugi Maru', sans-serif;
+    font-size: 22px;
+    font-weight: 500;
+    color: #333333;
 
 `
 
 const SecTitle = styled.div`
+    font-family  : 'Kosugi Maru', sans-serif;
+    font-size: 18px;
 
 `
 
 const SecSub = styled.div`
+    font-family  : 'Kosugi Maru', sans-serif;
+    font-size: 14px;
+    color: #7f7f7f;
+`
 
+const Input = styled.input`
+    font-family  : 'Kosugi Maru', sans-serif;
+    border: 1px solid #969696;
+    background-color: transparent;
+    appearance: none;
+    font-size: 18px;
+    padding: 2px;
+    line-height: 30px;
+    border-radius: 4px;
 `
 
 const TextField = styled.textarea`
+    font-family  : 'Kosugi Maru', sans-serif;
+    resize: none;
+    border-radius: 4px;
+    appearance: none;
+    border: 1px solid #969696;
+    font-size: 12px;
+    padding: 8px;
 `
+
+const Select = styled.select`
+    width: 40%;
+    line-height: 32px;
+    font-size: 22px;
+    padding: 4px;
+`
+
+const Option = styled.option`
+    font-size: 18px;
+`
+
+const Selector = ({options, onChange} : {options : string[], onChange: (val : string) => void}) => {
+    let optionList = [];
+    for(let option of options){
+        optionList.push(<Option value={option}>{option}</Option>);
+    }
+
+
+    return(
+        <Select onChange={(event)=>{onChange(event.target.value)}}>
+            {optionList}
+        </Select>
+    );
+}
 
 const PostBtn = styled.div`
     width            : 100%;
@@ -38,21 +86,21 @@ const PostBtn = styled.div`
     filter           : drop-shadow(0px 5px 2px rgba(0, 0, 0, 0.41));
     font-family      : 'Kosugi Maru', sans-serif;
     font-size        : 20px;
+    cursor: pointer;
 `
 
 const PostPage = () => {
     //TODO
-    // const onSubmit = () => {
-    //     const { error, isLoading } = usePostPrompt();
-        
-    // }
+    const onSubmit = () => {
+    }
 
-    const [title , setTitle] = useState<string>("");
+    const [title , setTitle]   = useState<string>("");
+    const [AIType, setAIType]  = useState<string>("");
+    const [prompt , setPrompt] = useState<string>("");
+    const [ans , setAns]       = useState<string>("");
+    const [memo , setMemo]     = useState<string>("");
 
-    const titleRef  = useRef<HTMLTextAreaElement>();
-    const promptRef = useRef<HTMLTextAreaElement>();
-    const ansRef    = useRef<HTMLTextAreaElement>();
-    const memoRef   = useRef<HTMLTextAreaElement>(); 
+
     
     return(
         <Back>
@@ -68,25 +116,57 @@ const PostPage = () => {
                     <Column padding={8}>
                         <SpaceBox height={16}/>
                             <PostTitle>新しいプロンプトを投稿する</PostTitle>
+
+                            <SpaceBox height={40}/>
                            
                             <SecTitle>タイトル</SecTitle>
                             <SecSub>プロンプトの内容を一言で</SecSub>
-                            <TextField></TextField>
+                            <SpaceBox height={8}/>
+                            <Input 
+                                onChange={(event)=>{setTitle(event.target.value)}}/>
+
+                            <SpaceBox height={32}/>
+
+                            <SecTitle>タイプ</SecTitle>
+                            <SecSub>どのAIを使った？</SecSub>
+                            <SpaceBox height={8}/>
+                            <Selector 
+                                options={["ChatGPT" ,"BARD"]}
+                                onChange={(val)=>{setAIType(val)}}/>
+
+                            <SpaceBox height={32}/>
+
 
                             <SecTitle>プロンプト</SecTitle>
                             <SecSub>AIになんと入力した？</SecSub>
-                            <TextField ></TextField>
+                            <SpaceBox height={8}/>
+                            <TextField 
+                                rows={20} 
+                                onChange={(event)=>{setPrompt(event.target.value)}}/>
+
+                            <SpaceBox height={32}/>
 
                             <SecTitle>返答</SecTitle>
                             <SecSub>AIはなんて返した？</SecSub>
+                            <SpaceBox height={8}/>
+                            <TextField 
+                                rows={20} 
+                                onChange={(event)=>{setAns(event.target.value)}}/>
+
+
+                            <SpaceBox height={32}/>
 
                             <SecTitle>メモ</SecTitle>
                             <SecSub>補足しておきたいこと</SecSub>
-                            <TextField></TextField>
+                            <SpaceBox height={8}/>
+                            <TextField 
+                                rows={8}
+                                onChange={(event)=>{setMemo(event.target.value)}}/>
                         <SpaceBox height={16}/>
                     </Column>
                 </Card>
-                <PostBtn>投稿する</PostBtn>
+                <PostBtn onClick={onSubmit}>投稿する</PostBtn>
+                <SpaceBox height={32}/>
             </Column>
         </Back>
     );
